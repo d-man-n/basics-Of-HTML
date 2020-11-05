d532e1bf865a6b8b8180caf420bd2153876115d8
root <root@web.morflot.local>
2020-11-05 13:41:42 +0300

    Add mobile menu

---
+++ js/mmenu.js
@@ -0,0 +1,22 @@
+let keys = {
+    ESC: 27,
+};
+const burger = document.querySelector(".header-burger");
+const menu_close = document.querySelector('.header-menu__close');
+const menu = document.querySelector('.mob-menu');
+
+function showMenu() {
+    menu.classList.add('mob-menu__showed');
+    document.addEventListener('keydown', function(e) {
+        if (e.keyCode = keys.ESC) {
+            closeMenu();
+        }
+    });
+}
+
+function closeMenu() {
+    menu.classList.remove('mob-menu__showed');
+}
+
+burger.addEventListener('click', showMenu);
+menu_close.addEventListener('click', closeMenu);
